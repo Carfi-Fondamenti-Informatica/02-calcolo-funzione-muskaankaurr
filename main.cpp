578a27974207b510c2059a716dbe53711711c4de
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-11-16 20:28:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+   float x=0, y=0, a=0, b=0, z=0;
+   cin >> a >> b >> x >> y;
+   
+   if (x<0 && y>0) {
+      cout << (z = a*x - b*y) << endl;
+   } else if (x>=0 && y<=0) {
+      cout << 8 z=a*x*x - b*y) << endl;
+   } else {
+      cout << (z=a*x + b*y*y) << endl;
+   }
+  
    return 0;
 }
